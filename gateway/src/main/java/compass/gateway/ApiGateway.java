fc62a317dd57bbe61e384a9baf11c30bc9f2adf0
Paulo Ricardo <paulo.rdndc@gmail.com>
2023-07-10 16:32:05 -0300

    Gateway Finalizado

--- gateway/src/main/java/compass/gateway/ApiGateway.java
+++ gateway/src/main/java/compass/gateway/ApiGateway.java
@@ -1,21 +1,31 @@
 package compass.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.logging.Logger;
+
 @Configuration
 public class ApiGateway {
 
+    @Autowired
+    private LoggingFilter loggingFilter;
+
     @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder buider){
-        return buider.routes()
-                .route("msnotification", r -> r
-                        .path("/v1")
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("msauthorization", r -> r.path("/oauth/**")
+                        .filters(f -> f.filter(loggingFilter))
+                        .uri("http://localhost:8081"))
+                .route("msnotification", r -> r.path("/notification/**")
+                        .filters(f -> f.filter(loggingFilter))
                         .uri("http://localhost:8082"))
-                .route("msproducts", r -> r
-                        .path("/v2")
+                .route("msproducts", r -> r.path("/products/**")
+                        .filters(f -> f.filter(loggingFilter))
                         .uri("http://localhost:8083"))
                 .build();
     }
